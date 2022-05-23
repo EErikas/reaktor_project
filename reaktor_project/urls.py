42bc64ad235e25acaa2eb99be5b47269cae81a75
EErikas <EErikas@users.noreply.github.com>
2022-05-23 15:51:21 +0300

    cleaned up urls file

--- reaktor_project/urls.py
+++ reaktor_project/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
 from django.urls import path
 from poetry_parser import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', views.package_list, name='package-list'),
     path('upload/', views.file_upload, name='upload'),
     path('package/<str:package_name>', views.package_page, name='package-page'),
