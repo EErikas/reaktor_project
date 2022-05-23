e42175db0a2e54fb5b2d9f140309aad51d24706e
EErikas <EErikas@users.noreply.github.com>
2022-05-23 12:23:08 +0300

    Cleaned up views

--- poetry_parser/views.py
+++ poetry_parser/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
 from django.http import JsonResponse
@@ -38,17 +38,7 @@ def file_upload(request):
             request.session['packages'] = data.get_packages()
             request.session['installed'] = data.get_installed()
             request.session['reverse_dependencies'] = data.get_reverse_dependencies()
-            # return JsonResponse(data.get_packages(), safe=False)
             return redirect('package-list')
-            # return render(request, 'packages.html', context={
-            #     'title': 'List of packages',
-            #     'packages': data.get_packages()
-            # })
-            # return JsonResponse(data.get_packages(), safe=False)
-
-        # print(data.get_packages())
-        # return JsonResponse(data.get_packages(), safe=False)
-    # else:
     return render(request, 'upload.html')
 
 
@@ -70,7 +60,7 @@ def package_page(request, package_name):
                      if package.get('name') == package_name]
     if len(matching_pkgs) == 0:
         messages.warning(request, f'Package "{package_name}" is not installed')
-        return redirect('upload')
+        return redirect('package-list')
     print(request.session.get('reverse_dependencies', {}).get(package_name))
     return render(request, 'package_page.html', context={
         'title': f'Details of {matching_pkgs[0].get("name")}',
